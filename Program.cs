60f99bb8e70e4f958c72d92b7d04267d92ffbfb6
Eduardo <eteixeira566@gmail.com>
2024-12-16 22:45:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using projeto_V1_mercado.Interface;
+using projeto_V1_mercado.Dados;
+using projeto_V1_mercado.Servicos;
+
+namespace projeto_V1_mercado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            VendaRepositorio vendaRepositorio = new VendaRepositorio();
+            ProdutoRepositorio produtoRepositorio = new ProdutoRepositorio();
+
+            RelatorioServico relatorioServico = new RelatorioServico(vendaRepositorio);
+            ProdutoServico produtoServico = new ProdutoServico(produtoRepositorio);
+            VendaServico vendaServico = new VendaServico(produtoRepositorio, vendaRepositorio);
+            Menu menu = new Menu(produtoServico, relatorioServico, vendaServico);
+            menu.ExibirMenu();
+
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
